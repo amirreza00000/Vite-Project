71ccec064be2d000783017e66c69e8543d5e1044
ya681 <abdyassin1391@gmail.com>
2025-03-16 11:20:49 +0330

    panel update

--- vite-project/src/components/common/panelSideBar/index.jsx
+++ vite-project/src/components/common/panelSideBar/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
-const sideBar = () => {
+import '..../index.css';
+const SideBar = () => {
   return (
-    <div>
+    <div className='w-483px height-973 bg-white rounded-sm'>
       
     </div>
   )
 }
 
-export default sideBar
+export default SideBar
---
+++ vite-project/src/screens/Panel/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SideBar from '../../components/common/panelSideBar'
+
+const panel = () => {
+  return (
+    <>
+      <SideBar/>
+    </>
+  )
+}
+
+export default panel
