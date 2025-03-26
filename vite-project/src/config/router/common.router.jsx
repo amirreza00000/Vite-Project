414c9c10ac02d27515c32fc07bdb5f12cea5267b
ya681 <abdyassin1391@gmail.com>
2025-03-26 20:16:34 +0330

    initial

--- vite-project/src/config/router/common.router.jsx
+++ vite-project/src/config/router/common.router.jsx
@@ -2,5 +2,5 @@ import { createBrowserRouter } from "react-router-dom";
 import Landing from "../../screens/Landing";
 export const commonRoutes=createBrowserRouter([
     {path:"/",element:<Landing/>},
-    {path:"/blogs",element:<Blog/>}
+    {path:"/blogsDetail/:id",element:<BlogDetail/>}
 ])
