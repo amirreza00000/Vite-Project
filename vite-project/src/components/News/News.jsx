d29701af4c80a4d7e9fae08d50c5977e06a321a7
ya681 <abdyassin1391@gmail.com>
2025-04-01 16:20:02 +0330

    responsived

--- vite-project/src/components/News/News.jsx
+++ vite-project/src/components/News/News.jsx
@@ -5,23 +5,23 @@ import instance from "../../core/services/interceptor";
 
 const News = ({news}) => {
   return (
-    <div className="mx-auto mb-11 md:mb-17 mt-25 md:mt-30 ">
-      <div className=" relative w-8/10  max-w-300 min-h-[304px] mx-auto rounded-4xl bg-[#F3F4F6] p-[25px] ">
-        <h2 className="whitespace-nowrap max-w-130 overflow-hidden text-ellipsis select-none m-0 inline text-[36px] absolute top-[-66px] right-[202px] md:right-[265px]">
+    <div className=" mx-auto mb-11 md:mb-17 mt-25 md:mt-30 ">
+      <div className=" relative w-9/10 md:w-8/10  max-w-300 md:min-h-[304px] mx-auto rounded-4xl bg-[#F3F4F6] p-[25px] ">
+        <h2 className="md:block whitespace-nowrap max-w-130 overflow-hidden text-ellipsis select-none m-0 inline text-[36px] absolute top-[-66px] right-0 md:right-[265px]">
           {news.title}
         </h2>
-        <div className="w-[180px] h-[150px] md:w-[248px] md:h-[210px] float-right"></div>
+        <div className="hidden md:block w-[180px] h-[150px] md:w-[248px] md:h-[210px] float-right"></div>
         <p className=" mx-auto  text-[20px] text-end md:text-[30px]">
           {news.miniDescribe}
         </p>
 
-        <div className="w-[250px] h-[250px] md:w-[300px] md:h-[300px] bg-[#9F9F9F] absolute top-[-72px] right-[-50px] rounded-4xl transform-[rotate(10deg)] translate-x-[20px]"></div>
+        <div className="hidden md:block w-[250px] h-[250px] md:w-[300px] md:h-[300px] bg-[#9F9F9F] absolute top-[-72px] right-[-50px] rounded-4xl transform-[rotate(10deg)] translate-x-[20px]"></div>
         <img
           alt="no image"
-          className="w-[250px] h-[250px] md:w-[300px] md:h-[300px] ml-[250px] bg-contain absolute top-[-72px] right-[-50px] rounded-4xl duration-1000 hover:transform-[scale(1.05)]"
+          className="hidden md:block w-[250px] h-[250px] md:w-[300px] md:h-[300px] ml-[250px] bg-contain absolute top-[-72px] right-[-50px] rounded-4xl duration-1000 hover:transform-[scale(1.05)]"
           src={!news.currentImage ? "/newspaper.png" : news.currentImage}
         />
-        <div className="absolute left-14 bottom-[-45px] -z-10 w-min-65 text-center p-4 pt-8 rounded-4xl bg-[#D1D1D1] whitespace-nowrap">
+        <div className="md:absolute md:left-14 md:bottom-[-45px] border-r-2 border-r-blue-400 rounded-0 md:border-0 -z-10 w-min-65 text-end p-4 mt-10 md:pt-8 md:rounded-4xl md:text-center md:bg-[#D1D1D1] md:whitespace-nowrap">
           {"نوشته شده توسط" + ":" + news.addUserFullName}
         </div>
       </div>
--- vite-project/src/components/News/NewsFromSameCategory/smallNews.jsx
+++ vite-project/src/components/News/NewsFromSameCategory/smallNews.jsx
@@ -1,18 +1,22 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-
-const SmallNews = ({imgSrc,detail,title,id}) => {
+const SmallNews = ({ imgSrc, detail, title, id }) => {
   return (
-    <Link to={"/blog/Detail/"+id} className="bg-[#F3F4F6] min-h-8 w-68 rounded-3xl *:relative *:bottom-10">
+    <Link
+      to={"/blog/Detail/" + id}
+      className="bg-[#F3F4F6] min-h-8 w-68 rounded-3xl *:relative *:bottom-10 mb-10 md:mb-0"
+    >
       <img
-        src={!imgSrc?"/newspaper.png":imgSrc}
+        src={!imgSrc ? "/newspaper.png" : imgSrc}
         alt="no image"
         className="h-60 w-60 rounded-3xl mx-auto"
       />
-      <h3 className="px-5 my-2 text-end overflow-hidden text-ellipsis">{title}</h3>
+      <h3 className="px-5 my-2 text-end overflow-hidden text-ellipsis">
+        {title}
+      </h3>
       <p className="px-6 py-1 max-h-25 overflow-hidden text-ellipsis">
-       {detail}
+        {detail}
       </p>
     </Link>
   );
